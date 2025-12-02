b7cb2da0f61d52c242a5b1cd787986bad66094de
Nicolas1993 <nicolasoalves1993@gmail.com>
2025-12-02 13:30:50 -0300

    Update dividir.c

--- dividir.c
+++ dividir.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+void main()
+{
+    int dividendo, divisor, quociente, resto;
+
+    printf("Dividendo: ");
+    scanf("%d", &dividendo);
+    printf("Divisor: ");
+    scanf("%d", &divisor);
+
+    if (divisor == 0) {
+        printf("Erro: divisão por zero não é permitida.\n");
+        return;
+    }
+
+    quociente = 0;
+    resto = dividendo;
+
+    while (resto >= divisor) {
+        resto = resto - divisor;
+        quociente++;
+    }
+
+    printf("\nO quociente da divisão inteira é: %d\n", quociente);
+    printf("O resto da divisão é: %d\n", resto);
+}
 
