078073954ab463d41667e9d591de3c28d8dce736
Nicolas1993 <nicolasoalves1993@gmail.com>
2025-12-08 01:41:55 -0300

    Update operacoesmatematicas_test.c

--- operacoesmatematicas_test.c
+++ operacoesmatematicas_test.c
@@ -5,12 +5,12 @@ int multiplicar(int a, int b);
 int dividir(int a, int b);
 
 int main() {
-
+    
     assert(multiplicar(3, 4) == 12);
-    assert(multiplicar(-2, 5) == -10);
+    assert(multiplicar(2, 5) == 10);
 
     assert(dividir(10, 2) == 5);
-    assert(dividir(-9, 3) == -3);
+    assert(dividir(9, 3) == 3);
 
     printf("Todos os testes passaram com sucesso!\n");
 
